fe0ed08c3e5cfa9282cbef200d7e179c1c5ee488
rlaqh <rlaqh1009@naver.com>
2021-05-12 13:55:27 +0900

    Android_Kakao_Pay_

---
+++ app/src/main/java/com/pay/MainActivity.java
@@ -0,0 +1,48 @@
+package com.pay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.kakao.sdk.common.util.Utility;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText editTextName;
+    EditText editTextPrice;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextName = findViewById(R.id.editName);
+        editTextPrice = findViewById(R.id.editPrice);
+
+        // 버튼 클릭 이벤트
+        Button button = findViewById(R.id.buttonPay);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // EditText에 입력한 상품 정보를 가져온다.
+                String name = editTextName.getText().toString();
+                String price = editTextPrice.getText().toString();
+
+                // 결제가 이루어지는 PayActivity를 생성한다.
+                // - 생성자를 이용하여 상품 정보를 입력한다.
+                PayActivity payActivity = new PayActivity(name, price);
+
+                // Intent로 새로운 Activity를 실행한다.
+                Intent intent = new Intent(getApplicationContext(), payActivity.getClass());
+                startActivity(intent);
+            }
+        });
+    }
+}
